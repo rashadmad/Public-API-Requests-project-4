082e0f108d68ed81670b18bbd2ba327e4cce5aae
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-05-24 22:40:54 -0700

    add image to card

--- public_api_request-v1/js/scripts.js
+++ public_api_request-v1/js/scripts.js
@@ -5,4 +5,10 @@ const createSearch = () => {
     $('.search-container').append('<input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">');
 }
 
+const createCard = (img,name) => {
+    //create a card and add a profile picture inside of it
+    $('.gallery').append('<img class="card-img" src=' + img + ' alt="profile picture of ' + name + '">')
+    //$('img').attr('src',img);
+}
+createCard("https://randomuser.me/api/portraits/men/8.jpg","Mike Gray")
 createSearch()
