07470be5fadbbcdc2738eda229847f9423b8b5bd
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-05-24 23:04:30 -0700

    complete foundation of createCard function

--- public_api_request-v1/js/scripts.js
+++ public_api_request-v1/js/scripts.js
@@ -5,10 +5,21 @@ const createSearch = () => {
     $('.search-container').append('<input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">');
 }
 
-const createCard = (img,name) => {
+const createCard = (name,img,location) => {
     //create a card and add a profile picture inside of it
-    $('.gallery').append('<img class="card-img" src=' + img + ' alt="profile picture of ' + name + '">')
-    //$('img').attr('src',img);
+    $('.gallery')
+    .append('<img class="card-img" src=' + img + ' alt="profile picture of ' + name + '">')
+    
+    //add a container for car info with an email, name and city
+    $('.gallery')
+    //add the card info container
+    .append('<div class="card-info-container"></div>')
+    //add the name
+    .append('<h3 id=' + name + 'class="card-name cap">' + name + '</h3>')
+    //add email
+    .append('<p class="card-text">' + name + '@awesomestartup.com</p>')
+    //add location
+    .append('<p class="card-text cap">' + location + '</p>')
 }
-createCard("https://randomuser.me/api/portraits/men/8.jpg","Mike Gray")
+createCard("Mike Gray","https://randomuser.me/api/portraits/men/19.jpg", "Chicago, Illinois", )
 createSearch()
