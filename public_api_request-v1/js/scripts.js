668272f58ff7313c2a303bed18e075b225eb3b79
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-05-24 22:10:20 -0700

    add search bar

--- public_api_request-v1/js/scripts.js
+++ public_api_request-v1/js/scripts.js
@@ -1,6 +1,8 @@
 "use strict";
-$(function () {
-    // Do stuff to the DOM
-    $('body').append('<p>Hello World!</p>');
-    console.log("greetings")
-});
+const createSearch = () => {
+    // create a form and add a seach bar and submit button to it
+    $('.search-container').append('<input type="search" id="search-input" class="search-input" placeholder="Search...">');
+    $('.search-container').append('<input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">');
+}
+
+createSearch()
