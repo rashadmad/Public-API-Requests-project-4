b121c67090e62de6ccea9f4c340dc4c706bd63b5
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-05-26 17:15:59 -0700

    implement instruction

--- public_api_request-v1/js/api.js
+++ public_api_request-v1/js/api.js
@@ -1,4 +1,4 @@
-//testing api connection
-  fetch('https://randomuser.me/api/?results=10')
+//Get and display 12 random users
+  fetch('https://randomuser.me/api/?results=12')
     .then(response => response.json())
     .then(data => createCard(data.results[0].name.first, data.results[0].picture.thumbnail, data.results[0].location.city))
